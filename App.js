3f4cb79f376b1dd859aad0351f087b5256eb064a
David Bergvik <davidbergvik@gmail.com>
2018-05-08 00:30:29 +0200

    Add loading logic to main component

--- App.js
+++ App.js
@@ -5,17 +5,37 @@ import './App.css';
 import Map from './components/Map/Map.js';
 import Loading from './components/Loading/Loading.js';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {coords: undefined};
+    this.getCoords();
+  }
   renderMap(coords){
     return coords ? (<Map mapCoords={coords}/>) : <Loading/>;
   }
+  fetchCoordsFromGPS() {
+    return new Promise(resolve => {
+      /*
+      Replace setTimeout below with your gps fetch logic.
+      Return response in resolve(your response)
+      */
+      setTimeout(() => {
+        resolve({x:10, y:20});
+      }, 2000);
+    });
+  }
+  async getCoords(){
+    console.log('fetching');
+    const coords = await this.fetchCoordsFromGPS();
+    console.log('coords found:',coords);
+    this.setState(previousState => {
+      return { coords: coords};
+    });
+  }
   render() {
-    const coords = {x:10, y:20}
+    const coords = this.state.coords
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         {this.renderMap(coords)}
 
       </div>
